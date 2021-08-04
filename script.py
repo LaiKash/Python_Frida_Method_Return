b8af2bdf3927e67813fd4a5e4d7fdbe42781bc9e
LaiKash <papicurri@gmail.com>
2021-08-04 18:04:31 +0100

    Update script.py

--- script.py
+++ script.py
@@ -9,62 +9,57 @@ parser.add_argument("cls",
                     help="Class name to attach Frida")
 parser.add_argument("mth",
                     help="Method name to call in Frida")
-parser.add_argument("file", default="arguments.txt", nargs='?', help="File name in this folder where the arguments to the method are stored.")
+parser.add_argument("file", default="arguments.txt", nargs='?',
+                     help="File name in this folder where the arguments to the method are stored (default name: arguments.txt).")
+parser.add_argument('-s', '--stdin', action="store_true",
+                    help='Read from stdin INSTEAD of the file.')
 
 
 args = parser.parse_args()
 
 
+def from_stdin(): 
+    script.on('message', on_message)
+    script.load()
+    for line in sys.stdin:
+        script.post({'type': 'cls', 'class': args.cls, 'meth': args.mth, 'line':line})
+        time.sleep(1)  # fails without this sleep
+        device.resume(pid)
+        sys.stdin.read()
+  
 
- 
-count = 0
-my_input = [ ]
-with open("arguments.txt") as fp:
-    while True:
-        count += 1
-        line = fp.readline()
- 
-        if not line:
-            break
-        my_input.append(line)
+def from_file():
+    count = 0
+    my_input = [ ]
+    with open("arguments.txt") as fp:
+        while True:
+            count += 1
+            line = fp.readline()
+            if not line:
+                break
+            my_input.append(line)
+    script.on('message', on_message)
+    script.load()
+    script.post({'type': 'cls', 'class': args.cls, 'meth': args.mth, 'line':my_input})
+    time.sleep(1)  # fails without this sleep
+    device.resume(pid)
+    sys.stdin.read()
 
 
 device = frida.get_usb_device()
 pid = device.spawn([args.pkg])
 session = device.attach(pid)
 
-
-script = session.create_script("""
-        var cl;
-        var meth;
-        var line;
-        var strArg = []
-        recv('cls', function onMessage(cls) { 
-            cl = cls.class;
-            meth = cls.meth;
-            strArg = cls.line;
-        });
-
-
-        Java.perform(function(){
-            for (let i = 0; i < strArg.length; i++) {
-                send(
-                     Java.use(cl)[meth](strArg[i])
-                );
-                }
-        });
-
-"""
-)
 def on_message(message, data):
     print(message["payload"])
-    
-    
-script.on('message', on_message)
-script.load()
-script.post({'type': 'cls', 'class': args.cls, 'meth': args.mth, 'line':my_input})
 
-time.sleep(1)  # fails without this sleep
-device.resume(pid)
-sys.stdin.read()
 
+if args.stdin:
+    with open('stdin.js') as f:
+        print("Wait for the APP to open and monitor if it crashes!! Then you can paste the lines :D")
+        script = session.create_script(f.read())
+        from_stdin()
+else:
+    with open('file.js') as f:
+        script = session.create_script(f.read())
+        from_file()  
