78769f3cf82810b6e1273332a7558df0a5b50f2d
LaiKash <papicurri@gmail.com>
2022-02-07 17:00:24 +0000

    Update script2.js

--- script2.js
+++ script2.js
@@ -15,6 +15,7 @@ Java.perform(function(){
     var decoderInstance = decoderClass.$new(context);
     for (let i = 0; i < strArg.length; i++) {
         strArg[i] = strArg[i].replace(/(\r\n|\n|\r)/gm, "");
+        console.log("\x1b[31m"+strArg[i]+"\x1b[0m")
         send(
             eval('decoderInstance.'+meth+'(\''+strArg[i]+'\')')
                 
