c9b882de3d776db15143e4adbee01016c2992452
LaiKash <papicurri@gmail.com>
2022-02-07 17:00:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ recv('cls', function onMessage(cls) {
 
 Java.perform(function(){
     strArg[i] = strArg[i].replace(/(\r\n|\n|\r)/gm, "");
-    console.log(strArg[i])
+    console.log("\x1b[31m"+strArg[i]+"\x1b[0m")
     for (let i = 0; i < strArg.length; i++) {
         send(
                 Java.use(cl)[meth](strArg[i])
